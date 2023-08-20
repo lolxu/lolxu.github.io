5908ab95326e195779ccb6b229a96ddfbd5cf37a
lolxu <davidx@usc.edu>
2023-08-19 17:20:08 -0700

    changed urls

--- scripts/project_btn.js
+++ scripts/project_btn.js
@@ -14,7 +14,7 @@ let ProjectText = [];
 
 for (let i = 0; i < ProjectNames.length; ++i) {
     ProjectText.push("");
-    ProjectText[i] = "./data/pages/projects/" + ProjectNames[i] + ".html";
+    ProjectText[i] = "./data/pages/projects/" + ProjectNames[i];
 
     // fetch("./data/pages/projects/" + ProjectNames[i] + ".html")
     // .then( r => r.text())
@@ -29,25 +29,4 @@ function loadProject(button, btnIndex) {
     if (button.getAttribute("class") == "btn btn-color-2 proj-expand-button") {
         window.location.href = ProjectText[btnIndex];
     }
-    // let projTextArea = document.getElementById("project_detailed_description");
-    // var x = document.getElementsByClassName("proj-expand-button-active");
-    // if (button.getAttribute("class") == "btn btn-color-2 proj-expand-button-active") {
-    //     x[0].className = "btn btn-color-2 proj-expand-button";
-    //     projTextArea.innerHTML = "";
-    //     projTextArea.style.opacity = 0;
-    //     // document.getElementById(button.getAttribute("id")).innerHTML = "Expand";
-    // }
-    // else {
-    //     if (x.length != 0) {
-    //         x[0].className = "btn btn-color-2 proj-expand-button";
-    //         projTextArea.innerHTML = "";
-    //         projTextArea.style.opacity = 0;
-    //         // document.getElementById(button.getAttribute("id")).innerHTML = "Expand";
-    //     }
-
-    //     projTextArea.innerHTML = ProjectText[btnIndex];
-    //     projTextArea.style.opacity = 1;
-    //     document.getElementById(button.getAttribute("id")).className = "btn btn-color-2 proj-expand-button-active";
-    //     // document.getElementById(button.getAttribute("id")).innerHTML = "Shrink";
-    // }
 }
